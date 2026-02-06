c439831829908095dca0ebef6569b1aac8b4bde1
Mouad Gourita <mouad.gourita@gmail.com>
2026-02-06 11:30:56 +0100

    feat: added Role to each user after Register

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $table = 'role';
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'role',
+        'users_id',
+    ];
 }
